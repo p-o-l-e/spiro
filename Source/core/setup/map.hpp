c28b28942f1d1f41f5844866f11d04d383531966
p-o-l-e <ant.float.farm@gmail.com>
2025-04-13 14:41:30 +0300

    ///

--- Source/core/setup/map.hpp
+++ Source/core/setup/map.hpp
@@ -1,49 +1,40 @@
 #pragma once
-
+#include <span>
 #include "../interface_headers.hpp"
 
 namespace core 
 {
     namespace settings 
     {
-        inline const descriptor_t descriptor_map[]
+        descriptor_t descriptor_map[]
         {
-            { interface::vco::descriptor    },
-            { interface::vco::descriptor    },
-            { interface::vco::descriptor    },
-            { interface::vco::descriptor    },
-
-            { interface::env::descriptor[0] },
-            { interface::env::descriptor[0] },
-            { interface::env::descriptor[0] },
-            { interface::env::descriptor[0] },
-
-            { interface::lfo::descriptor    },
-            { interface::lfo::descriptor    },
-
-            { interface::map::descriptor    },
-            { interface::map::descriptor    },
-
-            { interface::mix::descriptor    },
-
-            { interface::pdt::descriptor    },
-            { interface::sum::descriptor    },
-
-            { interface::rtr::descriptor    },
-
-            { interface::snh::descriptor[0] },
-            { interface::snh::descriptor[0] },
-
-            { interface::vca::descriptor[0] },
-            { interface::vca::descriptor[0] },
-
-            { interface::vcd::descriptor    },
-
-            { interface::vcf::descriptor    },
-            { interface::vcf::descriptor    },
-            
-            { interface::cro::descriptor    }
+            interface::vco::descriptor    ,
+            interface::vco::descriptor    ,
+            interface::vco::descriptor    ,
+            interface::vco::descriptor    ,
+            interface::env::descriptor[0] ,
+            interface::env::descriptor[0] ,
+            interface::env::descriptor[0] ,
+            interface::env::descriptor[0] ,
+            interface::lfo::descriptor    ,
+            interface::lfo::descriptor    ,
+            interface::map::descriptor    ,
+            interface::map::descriptor    ,
+            interface::mix::descriptor    ,
+            interface::pdt::descriptor    ,
+            interface::sum::descriptor    ,
+            interface::rtr::descriptor    ,
+            interface::snh::descriptor[0] ,
+            interface::snh::descriptor[0] ,
+            interface::vca::descriptor[0] ,
+            interface::vca::descriptor[0] ,
+            interface::vcd::descriptor    ,
+            interface::vcf::descriptor    ,
+            interface::vcf::descriptor    ,
+            interface::cro::descriptor    
         };
+
+        inline const std::span<descriptor_t> dm (descriptor_map);
 
         inline constexpr int n_modules()
         {
